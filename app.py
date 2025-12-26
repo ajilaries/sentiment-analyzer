bf9a2dd9739aa6a0337fc90c3a74713c2016f877
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-26 10:14:05 +0530

    finalized main file

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask , render_template , request     
+from flask import Flask, render_template, request
 from textblob import TextBlob
-app = Flask ( __name__ )
 
-@app.route ( '/' , methods = [ ' GET ' , ' POST ' ] )
-def index ():   
-    sentiment-""
-    text= ""
+app = Flask(__name__)
 
-    if request.method == ' POST ' :
-        text=request.form["message"]
-        analysis = TextBlob ( text )
+@app.route("/", methods=["GET", "POST"])
+def index():
+    sentiment = ""
+    text = ""
 
-        if analysis.sentiment.polarity > 0 :
-            sentiment = " Positive "
-        elif analysis.sentiment.polarity < 0 :
-            sentiment = " Negative "
-        else :
-            sentiment = " Neutral "     
-    return render_template ( ' index.html ' , sentiment = sentiment , text = text )
+    if request.method == "POST":
+        text = request.form["message"]
+        analysis = TextBlob(text)
 
-if __name__ == ' __main__ ' :
-    app.run ( debug = True )
+        if analysis.sentiment.polarity > 0:
+            sentiment = "Positive 😊"
+        elif analysis.sentiment.polarity < 0:
+            sentiment = "Negative 😠"
+        else:
+            sentiment = "Neutral 😐"
+
+    return render_template("index.html", sentiment=sentiment, text=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
